5a48d3d6500c650fca0dbfdb4f98ced23d637e23
Luis Diaz <licluisdiazm@hotmail.com>
2019-11-14 07:43:04 -0300

    Inicio

--- WpfAppAgencia/frmRegVehiculo.xaml.cs
+++ WpfAppAgencia/frmRegVehiculo.xaml.cs
@@ -79,6 +79,7 @@ namespace WpfAppAgencia
             {
                 IdMarc = Convert.ToInt32(((DataRowView)cboMarca.SelectedItem)["IdMarca"]);
                 cargaCboModelo(IdMarc);
+                cboModelo.Focus();
             }
         }
 
@@ -122,7 +123,7 @@ namespace WpfAppAgencia
             if (cboProcedencia.SelectedItem != null)
             {
                 IdProc = Convert.ToInt32(((DataRowView)cboProcedencia.SelectedItem)["IdProcedencia"]);
-                
+                dateInscripcion.Focus();
             }
         }
 
@@ -131,7 +132,7 @@ namespace WpfAppAgencia
             if (cboUso.SelectedItem != null)
             {
                 IdUso = Convert.ToInt32(((DataRowView)cboUso.SelectedItem)["IdUso"]);
-
+                txtCantPlacas.Focus();
             }
         }
 
@@ -140,8 +141,87 @@ namespace WpfAppAgencia
             if (cboTipo.SelectedItem != null)
             {
                 xIdTipo = Convert.ToInt32(((DataRowView)cboTipo.SelectedItem)["IdTipo"]);
-
+                txtMarcaChasis.Focus();
             }
+        }
+
+        private void txtDominio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                if (txtDominio.Text != string.Empty)
+                    cboMarca.Focus();
+                else
+                    MessageBox.Show("El Dominio no puede ser Nulo", "Mensaje", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void txtAnio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                if (txtAnio.Text != string.Empty)
+                    cboProcedencia.Focus();
+                else
+                    MessageBox.Show("El Año no puede ser Nulo", "Mensaje", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void dateInscripcion_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dateInscripcion.Text != string.Empty)
+                txtCodAutomotor.Focus();
+        }
+
+        private void txtMarcaMotor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                txtNroMotor.Focus();
+        }
+
+        private void txtNroMotor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                cboTipo.Focus();
+        }
+
+        private void txtMarcaChasis_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                txtNroChasis.Focus();
+        }
+
+        private void txtNroChasis_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                txtPeso.Focus();
+        }
+
+        private void dateAdquisicion_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dateAdquisicion.Text != string.Empty)
+                txtCarroceria.Focus();
+        }
+
+        private void txtCarroceria_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                txtCondicion.Focus();
+        }
+
+        private void txtPeso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                txtCarga.Focus();
+        }
+
+        private void txtCarga_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                dateAdquisicion.Focus();
+        }
+
+        private void txtCondicion_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                cboUso.Focus();
         }
     }
 }
